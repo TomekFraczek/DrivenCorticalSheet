d3f13546afc6a8201771ccbb7c6d2c503eef1adc
TomekBRL <tfraczek@uw.edu>
2021-09-10 14:24:17 -0700

    Filled in the code. We'll see it this works

--- smart_sweep.py
+++ smart_sweep.py
@@ -1,43 +1,89 @@
 import os
 import json
 
+import numpy as np
 from joblib import Parallel, delayed
 
+from plotting.plotformat import PlotSetup
 from main import model, save_data, plot
 
-def do_sweep():
-
-    # Prepare all folder destinations and configs
-
-    # Use sure run to run all points
 
 
+MAX_TRIES = 5
 
-def sure_run(point_list):
+
+def do_sweep(out_dir, config_name):
+
+    path_fmt = PlotSetup(out_dir, config_name)
+
+    with open('sweep_configs.json') as f:
+        sweep_config = json.load(f)[config_name]
+    all_points = prep_points(out_dir, sweep_config)
+
+    sure_run(path_fmt, all_points, 0)
+
+
+def prep_points(out_dir, sweep_config):
+    """Prepare all folders and the configurations for each of the points in the sweep"""
+    point_config = json.dumps(sweep_config['point_config'])
+
+    x_axis = np.linspace(**sweep_config['x-var'])
+    y_axis = np.linspace(**sweep_config['y-var'])
+    x_mesh, y_mesh = np.meshgrid(x_axis, y_axis)
+
+    points = []
+    for i, (x, y) in enumerate(zip(np.flatten(x_mesh), np.flatten(y_mesh))):
+        conf_here = json.loads(point_config.replace('<x-var>', x).replace('<y-var>', y))
+
+        point_name =  f'Point {i}'
+        point_dir = os.path.join(out_dir, point_name)
+        os.mkdir(point_dir)
+        with open(os.path.join(point_dir, 'config.json'), 'w') as f:
+            json.dump(conf_here, f)
+        points.append(point_name)
+    return points
+
+
+def sure_run(path_fmt, point_list, tries, n_jobs=-1):
 
     # Send command to run all points in parallel
+    point_paths = [os.path.join(path_fmt, point_list)]
+    Parallel(n_jobs=n_jobs, verbose=20)(
+        delayed(model)(p, path_fmt) for p in point_paths
+    )
 
     # Check which points failed to finish
+    incomplete = check_complete(path_fmt)
 
     # Recursively run all incomplete points
+    if tries <= MAX_TRIES and incomplete:
+        tries += 1
+        sure_run(path_fmt, incomplete, tries)
 
 
-def run_point():
-    ### This function should be run in parallel
+def run_point(point_path, fmt):
+    # This function should be run in parallel
 
     # Load config from file
+    with open(os.path.join(point_path, 'config.json')) as f:
+        config = json.load(f)
 
     # Run the simulation at this point and save the data
+    osc, time, fmt = model(config, fmt)
 
     # Check that the point has been run successfully
-        # If yes, make the file completion.txt with a note of the success
-        # If no, exit, logging whatever error to an error file
+    if time[-1] >= config['time']:
+        with open(fmt.file_name('completion', 'txt')) as f:
+            f.write('Simulation Completed')
+    else:
+        error_msg = 'Error unknown!'
+        with open(fmt.file_name('completion', 'txt')) as f:
+            f.write(f'Simulation failed to complete!\n    {error_msg}')
 
 
-def check_complete(out_dir):
+def check_complete(path_fmt):
 
-    folders_here = [f for f in out_dir.iterdir() if f.is_dir()]
+    folders_here = [f for f in path_fmt.directory.iterdir() if f.is_dir()]
     incomplete = [f for f in folders_here if not os.path.exists(os.path.join(f, 'completion.txt'))]
-
-    # return point IDS
+    return incomplete
 
