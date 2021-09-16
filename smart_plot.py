7c7e091e2d5a8479c44fb304bb21a2bf93a886ff
TomekBRL <tfraczek@uw.edu>
2021-09-16 12:43:57 -0700

    Added functions for mass individual plotting

---
+++ smart_plot.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+
+from plotting.plotformat import from_existing
+from plotting.animate import animate_one
+
+
+# Collection of all the functions to plot on each individual directory
+# Each entry should be the name of the primary output file as a key, with the function as the value
+PLOT_FUNCTIONS = {
+    'animation': animate_one
+}
+
+
+def completed_sims(source_dir):
+    return [d for d in from_existing(source_dir) if d.has_file('completion', 'txt')]
+
+
+def plot_individual(source_dir):
+
+    for sim_folder in completed_sims(source_dir):
+        for plot_name, plot_func in PLOT_FUNCTIONS.items():
+            if not sim_folder.has_file(plot_name):
+                plot_func(sim_folder)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Select model_config scenario & path (optional):')
+    parser.add_argument('--source', metavar='source directory',
+                        type=str, nargs='?',
+                        help='base path to source plot data from',
+                        default='plots')
+
+    args = parser.parse_args()
+    plot_individual(args.source)
+
+
