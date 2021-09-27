9e1486ebb833af988825213d3bff9031058fa031
TomekBRL <tfraczek@uw.edu>
2021-09-27 14:23:35 -0700

    Added basic 2d fourier plot function

--- plotting/fourier_space.py
+++ plotting/fourier_space.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from plotting import load_data
 from scipy.signal import welch
+from scipy.fft import fft2, fftfreq, fftshift
 
 
 def fourier_data_1d(data_src):
@@ -24,9 +25,30 @@ def fourier_data_1d(data_src):
 
     avg_psds = np.array(avg_psds)
 
-    saveable = np.array([*np.meshgrid(time, freqs), avg_psds])
+    saveable = np.array([avg_psds, *np.meshgrid(time, freqs)])
     np.save(data_src.file_name('1d_fourier_data', 'npy'), saveable)
     return avg_psds, time, freqs
+
+
+def fourier_data_2d(data_src):
+    config, osc_states, time, _ = load_data(data_src)
+
+    state_psds = []
+    for i, t in enumerate(time):
+        state = osc_states[:, :, i]
+
+        state_fft = fft2(state)
+        state_psd = fftshift(np.real(state_fft * np.conj(state_fft)))
+        state_psds.append(state_psd)
+
+    state_psds = np.array(state_psds)
+    n_osc = state.shape[-1]
+    raw_freqs = fftfreq(n_osc)
+    freqs = np.array([*raw_freqs[round(n_osc/2):], *raw_freqs[:round(n_osc/2)]])
+
+    saveable = np.array([state_psds, np.meshgrid(freqs, freqs)])
+    np.save(data_src.file_name('2d_fourier_data', 'npy'), saveable)
+    return state_psds, time, freqs
 
 
 def fourier_1d(data_src):
@@ -41,6 +63,24 @@ def fourier_1d(data_src):
     plt.xlabel('Time (s)')
     plt.tight_layout()
 
-
     plt.savefig(data_src.file_name('fourier 1d', 'png'))
 
+
+def fourier_2d(data_src):
+    state_ffts, time, freqs = fourier_data_2d(data_src)
+
+    fx, fy = np.meshgrid(freqs, freqs)
+
+    fig = plt.figure(figsize=(20, 13))
+    n_states = 6
+    for i in range(n_states):
+        plt.subplot(2, 3, i+1, aspect=1.0)
+        state_id = round((i / n_states) * len(time))
+        plt.pcolormesh(fx, fy, state_ffts[state_id, :, :])
+        plt.xlabel('X Frequencies')
+        plt.ylabel('Y Frequencies')
+        plt.colorbar()
+        plt.title(f'T = {round(time[state_id], 2)}')
+
+    plt.tight_layout()
+    plt.savefig(data_src.file_name('fourier 2d', 'png'))
